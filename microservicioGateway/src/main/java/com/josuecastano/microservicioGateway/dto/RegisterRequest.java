7851712bad9cc46bb072fd1e4811db4f72eb01d9
Dajumaks <Daniel09847montoya@gmail.com>
2025-11-29 17:44:38 -0500

    Implementación: Implementación del gateway y una parte de documentación con swagger

---
+++ microservicioGateway/src/main/java/com/josuecastano/microservicioGateway/dto/RegisterRequest.java
@@ -0,0 +1,14 @@
+package com.josuecastano.microservicioGateway.dto;
+
+import lombok.Data;
+
+import java.util.List;
+
+@Data
+public class RegisterRequest {
+    private String username;
+    private String email;
+    private String password_hash;
+    private Boolean isActive;
+    private List<String> roles;
+}
---
+++ microservicioGateway/src/main/java/com/josuecastano/microservicioGateway/repositories/UsuarioRepository.java
@@ -0,0 +1,10 @@
+package com.josuecastano.microservicioGateway.repositories;
+
+import com.josuecastano.microservicioGateway.entities.Usuario;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface UsuarioRepository extends JpaRepository<Usuario, String> {
+    Optional<Usuario> findByEmail(String email);
+}
